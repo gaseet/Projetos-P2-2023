c96f59b781f16ffba1015262d38840f82babb4ca
Humberto Lima <45747276+gaseet@users.noreply.github.com>
2023-10-03 12:37:26 -0300

    a

--- atividade12/14.c
+++ atividade12/14.c
@@ -2,40 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
-void gerarCartela(int cartela[5][5]) {
-    int numeros[25];
-    for (int i = 0; i < 25; i++) {
-        numeros[i] = i;
-    }
-
+int main() {
+    int matriz[5][5];
+    int usado[25] = {0}; // Inicializa o vetor 'usado' com zeros
     srand(time(NULL));
-    for (int i = 24; i > 0; i--) {
-        int j = rand() % (i + 1);
-        int temp = numeros[i];
-        numeros[i] = numeros[j];
-        numeros[j] = temp;
-    }
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            cartela[i][j] = numeros[i * 5 + j];
+    // Preenche a matriz
+    for(int i = 0; i < 5; i++) {
+        printf("i: %d\n", i);
+        for(int j = 0; j < 5; j++) {
+            printf("  j: %d\n", j);
+            int numero;
+            do {
+                printf("    Inside do-while\n");
+                numero = rand() % 99 + 1; // Gera um número aleatório de 1 a 99
+            } while (usado[numero - 1]); // Verifica se o número já foi usado
+            printf("    Got a valid number: %d\n", numero);
+            usado[numero - 1] = 1; // Marca o número como usado
+            matriz[i][j] = numero; // Atribui o número à matriz
         }
     }
-}
 
-void exibirCartela(int cartela[5][5]) {
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("%2d ", cartela[i][j]);
+    // Imprime a matriz
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            printf("%2d ", matriz[i][j]);
         }
         printf("\n");
     }
-}
-
-int main() {
-    int cartela[5][5];
-    gerarCartela(cartela);
-    exibirCartela(cartela);
 
     return 0;
 }
--- atividade12/teste.c
+++ atividade12/teste.c
@@ -15,7 +15,7 @@ int main() {
             int numero;
             do {
                 printf("    Inside do-while\n");
-                numero = rand() % 20 + 1; // Gera um número aleatório de 1 a 20
+                numero = rand() % 99 + 1; // Gera um número aleatório de 1 a 99
             } while (usado[numero - 1]); // Verifica se o número já foi usado
             printf("    Got a valid number: %d\n", numero);
             usado[numero - 1] = 1; // Marca o número como usado
