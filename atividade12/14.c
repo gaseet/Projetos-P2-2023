e32f9f88fbea62c013d2235aba22cd61ceb61614
gaseet <45747276+gaseet@users.noreply.github.com>
2023-10-22 13:12:59 -0300

    fix atv12 14.c

--- atividade12/14.c
+++ atividade12/14.c
@@ -1,33 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <time.h>
 
-int main() {
-    int matriz[5][5];
-    int usado[25] = {0};
-    srand(time(NULL));
+    int main() {
+        int matriz[5][5];
+        int usado[99] = {0};
+        srand(time(NULL));
 
-    for(int i = 0; i < 5; i++) {
-        printf("i: %d\n", i);
-        for(int j = 0; j < 5; j++) {
-            printf("  j: %d\n", j);
-            int numero;
-            do {
-                printf("    Inside do-while\n");
-                numero = rand() % 99 + 1;
-            } while (usado[numero - 1]);
-            printf("    Got a valid number: %d\n", numero);
-            usado[numero - 1] = 1;
-            matriz[i][j] = numero;
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                int numero;
+                do {
+                    numero = rand() % 99 + 1;
+                } while (usado[numero - 1]);
+                usado[numero - 1] = 1;
+                matriz[i][j] = numero;
+            }
         }
-    }
 
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
-            printf("%2d ", matriz[i][j]);
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                printf("%2d ", matriz[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
 
-    return 0;
-}
+        return 0;
+    }
