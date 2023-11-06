ed05903c6993e884ae535a8a779e3516b30a7b97
gaseet <45747276+gaseet@users.noreply.github.com>
2023-11-06 14:44:13 -0300

    atividade14

---
+++ atividade14/01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *numeros = (int *)malloc(5 * sizeof(int));
+
+    if (numeros == NULL) {
+        printf("Erro ao alocar memória. Encerrando o programa.\n");
+        return 1;
+    }
+
+    printf("Digite 5 números inteiros:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &numeros[i]);
+    }
+
+    printf("Os números digitados são: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", numeros[i]);
+    }
+    printf("\n");
+
+    free(numeros);
+
+    return 0;
+}
